9886837073943ad602478982dc57005bd0d8d4b1
Inácio Z. Vitkoski <65171054+inaciozacko@users.noreply.github.com>
2022-12-01 21:22:23 -0300

    Commit Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Modal';
+
+  tags: string[] = [];
+  selectedTag: string
+  linkImage: string;
+  linkImage2: string;
+  linkImage3: string;
+
+  statusCode: string;
+  mostrar: boolean = false;
+
+  constructor(
+    private appService: AppService
+  ) {}
+
+  ngOnInit(): void {
+    this.appService.getTags()
+    .subscribe(tags => {
+      this.tags = tags;
+    });
+  }
+
+  consultar() {
+    this.linkImage = 'https://http.dog/' + Number(this.statusCode)+'.jpg';
+  }
+  consultar2(){
+    this.linkImage2 = 'https://http.cat/' + Number(this.statusCode);
+  }
+  consultar3(){
+    this.linkImage3 = 'https://cataas.com/cat/' + this.selectedTag;
+  }
 }
